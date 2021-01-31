369e03f7020938f8a3daf57aaf8f5ea2000153a6
Danielle Barda <dbarda@redhat.com>
2021-01-31 10:10:28 +0200

    Remiving updating vm status after fixing a bug in CNV

--- pkg/managers/vm/vm.go
+++ pkg/managers/vm/vm.go
@@ -193,12 +193,6 @@ func (m *manager) updateVM(err error, virtualMachineFromMachine *kubevirtapiv1.V
 	previousResourceVersion := existingVM.ResourceVersion
 	virtualMachineFromMachine.ObjectMeta.ResourceVersion = previousResourceVersion
 
-	//TODO remove it after pushing that PR: https://github.com/kubevirt/kubevirt/pull/3889
-	virtualMachineFromMachine.Status = kubevirtapiv1.VirtualMachineStatus{
-		Created: existingVM.Status.Created,
-		Ready:   existingVM.Status.Ready,
-	}
-
 	updatedVM, err := m.updateInraClusterVM(virtualMachineFromMachine, machineScope)
 	if err != nil {
 		return false, nil, fmt.Errorf("failed to update VM: %w", err)
